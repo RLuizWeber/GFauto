450675a0d7873ef78a907405050e357cb20f7e88
RLuizWeber <rluizweber@yahoo.com.br>
2025-05-12 10:55:22 -0300

    Força novo build para carregar variáveis de ambiente atualizadas

--- app/api/webhook/mercadopago/route.ts
+++ app/api/webhook/mercadopago/route.ts
@@ -6,7 +6,7 @@ import { Resend } from "resend";
 
 const prisma = new PrismaClient();
 
-// Configuração do Mercado Pago Atual
+// Configuração do Mercado Pago Atual mais Recente
 const mpClient = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
 const mercadopagoPayment = new MercadoPagoPayment(mpClient);
 const mercadopagoMerchantOrder = new MerchantOrder(mpClient);
