abde875f830f88b9f11f9cb368ef3ed0ec9ba032
RLuizWeber <rluizweber@yahoo.com.br>
2025-05-23 11:41:08 -0300

    Atualiza next.config.js para resolver problema de build na Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    // Habilitar cache para melhorar o tempo de build
+    turboDeps: true,
+  },
+  // Garantir que o painel administrativo seja incluído na build
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+}
 
 module.exports = nextConfig;
