9153464985706fc4c918f96c29fbb7e24a26ebc1
RLuizWeber <rluizweber@yahoo.com.br>
2025-07-21 09:56:20 -0300

    atualização Prisma 6

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-// Habilitar cache para melhorar o tempo de build	  
-    turboDeps: true,
-  },
-// Garantir que o painel administrativo seja incluído na build
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-// Configuração para o deploy na Vercel
-  distDir: '.next',
-  // ADICIONAR ESTA CONFIGURAÇÃO:
-  serverComponentsExternalPackages: ['@prisma/client'],
+    reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    distDir: '.next'
 }
 
 module.exports = nextConfig
